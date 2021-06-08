34cf3d132ffcf2c2da7968eba568e6676b1a0a6d
nokindj <nokindj@gmail.com>
2021-06-08 17:30:57 +0100

    finished footer section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Dropdown from "./components/Dropdown";
+import Hero from "./components/Hero";
+import InfoSection from "./components/InfoSection";
+import Navbar from "./components/Navbar";
+import { SliderData } from "./data/SliderData";
+import { InfoData, InfoDataTwo } from "./data/InfoData";
+import GlobalStyle from "./globalStyles";
+import Listings from "./components/Listings";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <Hero slides={SliderData} />
+      <InfoSection {...InfoData} />
+      <Listings />
+      <Features />
+      <InfoSection {...InfoDataTwo} />
+      <Footer />
+    </>
   );
 }
 
